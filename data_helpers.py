3b2ae50a96d954544e20bee0ff362b1f3a03a81e
ianbstewart <ianbstew@lit1000.eecs.umich.edu>
2021-11-19 02:57:25 -0500

    updated LM queries

--- data_helpers.py
+++ data_helpers.py
@@ -4,31 +4,34 @@ from unidecode import unidecode
 import re
 
 VOWEL_MATCHER = re.compile('^[aeiou].+')
-def build_relationship_target_sentence(lang, subject_word, sentence, article_pronoun_lookup, poss_pronoun_lookup, relationship_word_gender, relationship_word):
-    poss_pronoun = poss_pronoun_lookup[relationship_word_gender]
-    subject_word_pronoun = article_pronoun_lookup[relationship_word_gender]
+def build_relationship_target_sentence(lang, subject_word, sentence, article_pronoun_lookup, poss_pronoun_lookup, relationship_word_gender, relationship_word, subject_word_gender=None):
+    target_poss_pronoun = poss_pronoun_lookup[relationship_word_gender]
+    if(lang == 'en'):
+        target_poss_pronoun = poss_pronoun_lookup[subject_word_gender]
+    subject_word_article = article_pronoun_lookup[subject_word_gender]
     # clean up pronouns: catch vowels
     clean_subject_word = unidecode(subject_word)
     if(VOWEL_MATCHER.match(clean_subject_word)):
         if(lang == 'fr' or lang=='it'):
-            subject_word_pronoun = "l'"
+            subject_word_article = "l'"
     clean_relationship_word = unidecode(relationship_word)
     if(VOWEL_MATCHER.match(clean_relationship_word)):
         if(lang == 'fr'):
-            poss_pronoun = "son"
+            target_poss_pronoun = "son"
     # add extra space for pronoun
-    if(subject_word_pronoun != "l'"):
-        subject_word_pronoun += ' '
-    subject_word_NP = f'{subject_word_pronoun}{subject_word}'
+    if(subject_word_article != "l'"):
+        subject_word_article += ' '
+    subject_word_NP = f'{subject_word_article}{subject_word}'
     replacement_pairs = [
-        ('X', subject_word_NP), ('PRON', poss_pronoun), ('Y', relationship_word)
+        ('X', subject_word_NP), ('PRON', target_poss_pronoun), ('Y', relationship_word)
     ]
     for x, y in replacement_pairs:
         sentence = sentence.replace(x,y)
     return sentence
 
+OCCUPATION_NON_GENDER_LANGS={'en'}
 def generate_occupation_relationship_sentence_data(relationship_sents, occupation_words, relationship_words, 
-                                                   lang_art_PRON_lookup, lang_POSS_PRON_lookup, 
+                                                   lang_art_PRON_lookup, lang_POSS_PRON_lookup,
                                                    relationship_type='same_gender', langs=['es']):
     data = []
     for lang_i in langs:
@@ -36,24 +39,30 @@ def generate_occupation_relationship_sentence_data(relationship_sents, occupatio
         poss_pronoun_lookup_i = lang_POSS_PRON_lookup[lang_i]
         sents_i = relationship_sents.loc[:, f'{lang_i}_sentence'].values
         # limit to occupation words that have different forms for female/male
-        occupation_words_i = occupation_words[occupation_words.loc[:, f'{lang_i}_female']!=occupation_words.loc[:, f'{lang_i}_male']]
+        if(lang_i not in OCCUPATION_NON_GENDER_LANGS):
+            occupation_words_i = occupation_words[occupation_words.loc[:, f'{lang_i}_female']!=occupation_words.loc[:, f'{lang_i}_male']]
+        else:
+            occupation_words_i = occupation_words.copy()
         female_occupation_words_i = occupation_words_i.loc[:, f'{lang_i}_female'].values
         male_occupation_words_i = occupation_words_i.loc[:, f'{lang_i}_male'].values
         female_relationship_words_i = relationship_words.loc[:, f'{lang_i}_female'].values
         male_relationship_words_i = relationship_words.loc[:, f'{lang_i}_male'].values
         if(relationship_type=='same_gender'):
-            male_subject_occupation_relationship_combos_i = list(product(male_occupation_words_i, male_relationship_words_i))
-            female_subject_occupation_relationship_combos_i = list(product(female_occupation_words_i, female_relationship_words_i))
+            male_subject_occupation_relationship_combos_i = [(x[0], x[1], 'male') for x in list(product(male_occupation_words_i, male_relationship_words_i))]
+            female_subject_occupation_relationship_combos_i = [(x[0], x[1], 'female') for x in list(product(female_occupation_words_i, female_relationship_words_i))]
         else:
-            male_subject_occupation_relationship_combos_i = list(product(male_occupation_words_i, female_relationship_words_i))
-            female_subject_occupation_relationship_combos_i = list(product(female_occupation_words_i, male_relationship_words_i))
+            male_subject_occupation_relationship_combos_i = [(x[0], x[1], 'female') for x in list(product(male_occupation_words_i, female_relationship_words_i))]
+            female_subject_occupation_relationship_combos_i = [(x[0], x[1], 'male') for x in list(product(female_occupation_words_i, male_relationship_words_i))]
+
         for sent_j in sents_i:
-            for subject_word_k, relationship_word_k in male_subject_occupation_relationship_combos_i:
-                final_sent_j = build_relationship_target_sentence(lang_i, subject_word_k, sent_j, article_pronoun_lookup_i, poss_pronoun_lookup_i, 'male', relationship_word_k)
-                data.append([final_sent_j, lang_i, subject_word_k, relationship_word_k, 'male'])
-            for subject_word_k, relationship_word_k in female_subject_occupation_relationship_combos_i:
-                final_sent_j = build_relationship_target_sentence(lang_i, subject_word_k, sent_j, article_pronoun_lookup_i, poss_pronoun_lookup_i, 'female', relationship_word_k)
-                data.append([final_sent_j, lang_i, subject_word_k, relationship_word_k, 'female'])
+            subject_word_gender_i = 'male'
+            for subject_word_k, relationship_word_k, relationship_gender_k in male_subject_occupation_relationship_combos_i:
+                final_sent_j = build_relationship_target_sentence(lang_i, subject_word_k, sent_j, article_pronoun_lookup_i, poss_pronoun_lookup_i, relationship_gender_k, relationship_word_k, subject_word_gender=subject_word_gender_i)
+                data.append([final_sent_j, lang_i, subject_word_k, relationship_word_k, subject_word_gender_i])
+            subject_word_gender_i = 'female'
+            for subject_word_k, relationship_word_k, relationship_gender_k in female_subject_occupation_relationship_combos_i:
+                final_sent_j = build_relationship_target_sentence(lang_i, subject_word_k, sent_j, article_pronoun_lookup_i, poss_pronoun_lookup_i, relationship_gender_k, relationship_word_k, subject_word_gender=subject_word_gender_i)
+                data.append([final_sent_j, lang_i, subject_word_k, relationship_word_k, subject_word_gender_i])
     data = pd.DataFrame(data, columns=['sent', 'lang', 'subject_word', 'relationship_word', 'subject_gender'])
     other_gender_lookup = {'male' : 'female', 'female' : 'male'}
     if(relationship_type=='same_gender'):
@@ -64,9 +73,14 @@ def generate_occupation_relationship_sentence_data(relationship_sents, occupatio
 
 def load_relationship_occupation_template_data():
     occupation_words = pd.read_csv('data/multilingual_gender_occupations.tsv', sep='\t', index_col=False)
+    # copy English occupations => male/female columns
+    occupation_words = occupation_words.assign(**{
+        'en_male' : occupation_words.loc[:, 'en'].values,
+        'en_female': occupation_words.loc[:, 'en'].values,
+    })
     relationship_words = pd.read_csv('data/multilingual_relationship_words.tsv', sep='\t', index_col=False)
     relationship_sents = pd.read_csv('data/multilingual_relationship_sentences.tsv', sep='\t', index_col=False)
-    langs = ['es', 'fr', 'it']
+    langs = ['es', 'fr', 'it', 'en']
     lang_art_PRON_lookup = {
         'es' : {
             'female' : 'la',
@@ -79,6 +93,10 @@ def load_relationship_occupation_template_data():
         'it' : {
             'female' : 'la',
             'male' : 'il',
+        },
+        'en' : {
+            'female' : 'the',
+            'male' : 'the',
         }
     }
     lang_POSS_PRON_lookup = {
@@ -93,6 +111,10 @@ def load_relationship_occupation_template_data():
         'it' : {
             'female' : 'la sua',
             'male' : 'il suo',
+        },
+        'en' : {
+            'female' : 'her',
+            'male' : 'his'
         }
     }
     return occupation_words, relationship_words, relationship_sents, langs, lang_art_PRON_lookup, lang_POSS_PRON_lookup    
@@ -121,9 +143,8 @@ def extract_subject_relationship_gender(data, relationship_words):
         'translation_relationship_gender' : data.loc[:, 'translation_txt'].apply(lambda x: unidecode(x)).apply(lambda x: relationship_gender_lookup[relationship_word_matcher.search(x).group(0)] if relationship_word_matcher.search(x) is not None else None),
     })
     return data
-def translate_subject_relationship_words(data, occupation_words, relationship_words):
+def translate_subject_relationship_words(data, occupation_words, relationship_words, langs=['es', 'fr', 'it']):
     word_other_en_lookup = {}
-    langs = ['es', 'fr', 'it']
     en_occupation_words = occupation_words.loc[:, 'en'].values.tolist()
     en_female_relationship_words = relationship_words.loc[:, 'en_female'].apply(unidecode).values.tolist()
     en_male_relationship_words = relationship_words.loc[:, 'en_male'].apply(unidecode).values.tolist()
