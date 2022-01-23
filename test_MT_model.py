bdcf88358e9a195a3bba767dd4a55be66d75d80e
istewart6 <istewart6@gatech.edu>
2022-01-23 11:35:11 -0500

    added test code

---
+++ test_MT_model.py
@@ -0,0 +1,49 @@
+"""
+Test fully-trained MT model.
+"""
+import os
+from argparse import ArgumentParser
+
+import pandas as pd
+import torch
+from transformers import MBartTokenizer, MBartForConditionalGeneration
+from datasets import load_from_disk
+from tqdm import tqdm
+
+def __main__():
+    parser = ArgumentParser()
+    parser.add_argument('model_dir')
+    parser.add_argument('out_dir')
+    parser.add_argument('test_data_dir')
+    parser.add_argument('--device_id', type=int, default=0)
+    args = vars(parser.parse_args())
+    model_dir = args['model_dir']
+    out_dir = args['out_dir']
+    test_data_dir = args['test_data_dir']
+    device_id = args['device_id']
+
+    # load model
+    model = MBartForConditionalGeneration.from_pretrained(model_dir)
+    tokenizer = MBartTokenizer.from_pretrained(model_dir)
+    test_data = load_from_disk(test_data_dir)
+    model.eval()
+    device = f'cuda:{device_id}'
+    model.to(device)
+
+    # generate
+    test_cols = ['input_ids', 'attention_mask']
+    with torch.no_grad():
+        test_pred_output = [model.generate(**{c: x[c].to(device).unsqueeze(0) for c in test_cols}) for x in tqdm(test_data)]
+    # write to file
+    test_file = os.path.join(out_dir, 'test_data_output.gz')
+    test_input = tokenizer.batch_decode(test_data['input_ids'], skip_special_tokens=True)
+    test_output = tokenizer.batch_decode(test_data['labels'])
+    test_output_data = pd.DataFrame([test_input, test_output, test_pred_output], columns=['input', 'output', 'pred'])
+    test_output_data.to_csv(test_file, sep='\t', compression='gzip', index=False)
+
+    # compute accuracy metrics
+    # bleu_metric = load_metric('BLEU')
+    # rouge_metric = load_metric('rouge')
+
+if __name__ == '__main__':
+    main()
---
+++ test_MT_model.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --job-name=test_MT_model
+#SBATCH --mail-type=BEGIN,END
+#SBATCH --output=/home/%u/logs/%x-%j.log
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem-per-gpu=30g
+#SBATCH --time=2:00:00
+#SBATCH --partition=gpu
+#SBATCH --gpus=1
+#SBATCH --account=mihalcea0
+LANG=es
+MODEL_DIR=finetune_translate_mbart_lang="$LANG"
+OUT_DIR=data/MT/lang_"$LANG"
+TEST_DATA_DIR="$OUT_DIR"/test_data
+# offline transformers on server
+export TRANSFORMERS_OFFLINE=1
+export HF_DATASETS_OFFLINE=1
+python test_MT_model.py $MODEL_DIR $OUT_DIR $TEST_DATA_DIR
