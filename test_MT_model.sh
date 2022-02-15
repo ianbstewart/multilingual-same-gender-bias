2bdfcdab7293b316b940282029b086eea17b89b3
ianbstewart <ianbstew@umich.edu>
2022-02-15 16:01:26 -0500

    added MT metrics

--- test_MT_model.sh
+++ test_MT_model.sh
@@ -10,17 +10,21 @@
 #SBATCH --partition=gpu
 #SBATCH --gpus=1
 #SBATCH --account=mihalcea0
-SOURCE_LANG=es
+#SOURCE_LANG=es
 #SOURCE_LANG=fr
-#SOURCE_LANG=it
-MODEL_DIR=finetune_translate_mbart_lang="$SOURCE_LANG"/finetune/checkpoint-1000/
-# default data
-#OUT_DIR=data/MT/data_"$SOURCE_LANG"
-#TEST_DATA_DIR="$OUT_DIR"/test_data
+SOURCE_LANG=it
+# default data: test data
+TEST_DATA_DIR=data/MT/data_"$SOURCE_LANG"/test_data
+# default data: original model
+MODEL_DIR=finetune_translate_mbart_lang="$SOURCE_LANG"/checkpoint-54000/
+OUT_DIR=data/MT/data_"$SOURCE_LANG"
+# default data: fine-tuned model
+#MODEL_DIR=finetune_translate_mbart_lang="$SOURCE_LANG"/finetune/checkpoint-1500/
+#OUT_DIR=data/MT/data_"$SOURCE_LANG"/translation_data_type=relationship_finetune/
 # diff/same gender data
 #OUT_DIR=data/MT/translation_data_type=relationship_lang="$SOURCE_LANG"/
-OUT_DIR=data/MT/translation_data_type=relationship_lang="$SOURCE_LANG"/finetune/
-TEST_DATA_DIR=data/MT/translation_data_type=relationship_lang="$SOURCE_LANG"/data_"$SOURCE_LANG"/test_data/
+#OUT_DIR=data/MT/translation_data_type=relationship_lang="$SOURCE_LANG"/finetune/
+#TEST_DATA_DIR=data/MT/translation_data_type=relationship_lang="$SOURCE_LANG"/data_"$SOURCE_LANG"/test_data/
 # offline transformers on server
 export TRANSFORMERS_OFFLINE=1
 export HF_DATASETS_OFFLINE=1
