6835e5c5526e27097ae27d3f84345963d033d02a
ianbstewart <ianbstew@lit1000.eecs.umich.edu>
2022-03-20 23:30:57 -0400

    hard coding relationship sent collecftion

---
+++ collect_parse_relationship_sents_in_corpus.sh
@@ -0,0 +1,10 @@
+# ES
+DATA_FILE=data/wiki/eswiki-20181001-corpus.xml.bz2
+LANG='es'
+# FR
+#DATA_FILE=data/wiki/frwiki-20181001-corpus.xml.bz2
+#LANG='fr'
+# IT
+#DATA_FILE=data/wiki/itwiki-20181001-corpus.xml.bz2
+#LANG='it'
+python collect_parse_relationship_sents_in_corpus.py $DATA_FILE $LANG
