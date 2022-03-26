dc9b6bae9a7be53f628a2854e6720b9da38a3050
ianbstewart <ianbstew@umich.edu>
2022-03-26 13:49:15 -0400

    merging

--- collect_parse_relationship_sents_in_corpus.sh
+++ collect_parse_relationship_sents_in_corpus.sh
@@ -2,9 +2,9 @@
 #DATA_FILE=data/wiki/eswiki-20181001-corpus.xml.bz2
 #LANG='es'
 # FR
-DATA_FILE=data/wiki/frwiki-20181001-corpus.xml.bz2
-LANG='fr'
+#DATA_FILE=data/wiki/frwiki-20181001-corpus.xml.bz2
+#LANG='fr'
 # IT
-#DATA_FILE=data/wiki/itwiki-20181001-corpus.xml.bz2
-#LANG='it'
+DATA_FILE=data/wiki/itwiki-20181001-corpus.xml.bz2
+LANG='it'
 python collect_parse_relationship_sents_in_corpus.py $DATA_FILE $LANG
